bd352b0500de99bb5351497d72f4baab6e1b6a77
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-04-23 09:16:12 -0700

    Write #insert and #poll methods for Heap class

--- src/Heap.ts
+++ src/Heap.ts
@@ -13,6 +13,49 @@ class Heap<T> {
 		this._compare = compare;
 		this._items = []; 
 	}
+
+	get size(): number {
+		return this._items.length; 
+	}
+
+	public peek(): T {
+		this._throwErrorIfEmptyHeap; 
+
+		return this._items[0];
+	}
+
+	public insert(element: T): void {
+		this._items.push(element); 
+		this._heapifyUp(); 
+	}
+
+	public poll(): T | undefined {
+		this._throwErrorIfEmptyHeap();
+
+		// store copy of root element
+		const element: T = this._items[0]; 
+		// move last element to root since root element is being removed
+		this._items[0] = this._items[this.size - 1]; 
+		// remove last element since it's already at root
+		this._items.pop(); 
+		// ensure heap invariant is upheld
+		this._heapifyDown();
+
+		return element; 
+	}
+
+	private _heapifyUp(): void {
+
+	}
+
+	private _heapifyDown(): void {
+
+	}
+
+	private _throwErrorIfEmptyHeap(): void {
+		if (this.size === 0) 
+			throw new Error("Heap is empty"); 
+	}
 }
 
 // const heap = new Heap((a, b) => 1);
